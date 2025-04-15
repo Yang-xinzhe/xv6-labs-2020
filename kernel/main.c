c85c0045df94c2c01f9acc6ad43c70e33fe9e914
Yangxinzhe <1245314699@qq.com>
2025-04-15 16:50:18 +0800

    feat: physical page allocator

--- kernel/main.c
+++ kernel/main.c
@@ -13,7 +13,14 @@ void main() {
         consoleinit();
         printfinit();
         printf("\n");
-        printf("Yangxinzhe kernel is booting\n");
+        printf("Yangxinzhe kernel is booting in hart %d \n", cpuid());
         printf("\n");
+        kinit();            // physical page allocator  
+        started = 1;     
+    } else {
+        while(started == 0)
+            ;
+        __sync_synchronize();
+        printf("hart %d starting \n", cpuid());
     }
 }
