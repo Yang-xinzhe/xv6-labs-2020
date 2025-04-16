bf317237278708f3fde8204473313692e6b71e83
Yangxinzhe <1245314699@qq.com>
2025-04-16 17:07:42 +0800

    feat: interface update

--- kernel/defs.h
+++ kernel/defs.h
@@ -5,13 +5,34 @@ struct context;
 void            consoleinit(void);
 void            consputc(int);
 
+// kalloc.c
+void*           kalloc(void);
+void            kfree(void *);
+void            kinit(void);
+
 // proc.c
 int             cpuid(void);
+void            exit(int);
+int             fork(void);
+int             growproc(int);
+pagetable_t     proc_pagetable(struct proc *);
+void            proc_freepagetable(pagetable_t, uint64);
+int             kill(int);
 struct cpu*     mycpu(void);
+struct cpu*     getmycpu(void);
 struct proc*    myproc();
+void            procinit(void);
+void            scheduler(void) __attribute__((noreturn));
 void            sched(void);
-void sleep(void *chan, struct spinlock *lk);
-void wakeup(void *chan);
+void            setproc(struct proc*);
+void            sleep(void*, struct spinlock*);
+void            userinit(void);
+int             wait(uint64);
+void            wakeup(void*);
+void            yield(void);
+int             either_copyout(int user_dst, uint64 dst, void *src, uint64 len);
+int             either_copyin(void *dst, int user_src, uint64 src, uint64 len);
+void            procdump(void);
 
 // printf.c
 void            printf(char*, ...);
@@ -35,3 +56,32 @@ void            pop_off(void);
 void            uartinit(void);
 void            uartputc(int);
 void            uartputc_sync(int);
+
+// string.c
+int             memcmp(const void*, const void*, uint);
+void*           memmove(void*, const void*, uint);
+void*           memset(void*, int, uint);
+char*           safestrcpy(char*, const char*, int);
+int             strlen(const char*);
+int             strncmp(const char*, const char*, uint);
+char*           strncpy(char*, const char*, int);
+
+
+// vm.c
+void            kvminit(void);
+void            kvminithart(void);
+uint64          kvmpa(uint64);
+void            kvmmap(uint64, uint64, uint64, int);
+int             mappages(pagetable_t, uint64, uint64, uint64, int);
+pagetable_t     uvmcreate(void);
+void            uvminit(pagetable_t, uchar *, uint);
+uint64          uvmalloc(pagetable_t, uint64, uint64);
+uint64          uvmdealloc(pagetable_t, uint64, uint64);
+int             uvmcopy(pagetable_t, pagetable_t, uint64);
+void            uvmfree(pagetable_t, uint64);
+void            uvmunmap(pagetable_t, uint64, uint64, int);
+void            uvmclear(pagetable_t, uint64);
+uint64          walkaddr(pagetable_t, uint64);
+int             copyout(pagetable_t, uint64, char *, uint64);
+int             copyin(pagetable_t, char *, uint64, uint64);
+int             copyinstr(pagetable_t, char *, uint64, uint64);
