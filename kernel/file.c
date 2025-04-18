da92f8e565737f5e276de81ba58adff3534f7118
yangxinzhe <yangxinzhe>
2025-04-18 18:03:06 +0800

    feat: fileinit

--- kernel/file.c
+++ kernel/file.c
@@ -1,7 +1,18 @@
 #include "types.h"
 #include "param.h"
+#include "riscv.h"
+#include "fs.h"
 #include "spinlock.h"
 #include "defs.h"
 #include "file.h"
+#include "stat.h"
 
 struct devsw devsw[NDEV]; 
+struct {
+    struct spinlock lock;
+    struct file file[NFILE];
+} ftable;
+
+void fileinit(void) {
+    initlock(&ftable.lock, "ftable");
+}
