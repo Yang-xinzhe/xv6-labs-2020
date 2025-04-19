1b132e8b39fbc73dd188de2deaae4f7699c551be
Yangxinzhe <1245314699@qq.com>
2025-04-19 19:34:34 +0800

    style: change what?

--- kernel/string.c
+++ kernel/string.c
@@ -58,3 +58,35 @@ int strncmp(const char *p, const char *q, uint n){
     }
     return (uchar)*p - (uchar)*q;
 }
+
+char* strncpy(char *s, const char *t, int n) {
+    char *os;
+
+    os = s;
+    while(n-- > 0 && (*s++ = *t++) != 0)
+        ;
+    while(n-- > 0)
+        *s++ = 0;
+    return os;
+}
+
+// Like strncpy but guaranteed to NUL-terminate.
+char *safestrcpy(char *s, const char *t, int n) {
+    char *os;
+
+    os = s;
+    if(n <= 0)
+        return os;
+    while(--n > 0 && (*s++ = *t++) != 0)
+        ;
+    *s = 0;
+    return os;
+}
+
+int strlen(const char *s) {
+    int n;
+
+    for(n = 0 ; s[n] ; n++)
+        ;
+    return n;
+}
