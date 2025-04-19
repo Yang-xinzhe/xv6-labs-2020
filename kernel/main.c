c5dbf2a0bbb86f6f913b8fa87914c4753b2335e0
Yangxinzhe <1245314699@qq.com>
2025-04-19 19:30:35 +0800

    feat: completed

--- kernel/main.c
+++ kernel/main.c
@@ -25,11 +25,19 @@ void main() {
         binit();            // buffer cache
         iinit();            // inode cache
         fileinit();         // file table
+        virtio_disk_init(); // emulated hard disk
+        userinit();         // first user process
+        __sync_synchronize();
         started = 1;     
     } else {
         while(started == 0)
             ;
         __sync_synchronize();
         printf("hart %d starting \n", cpuid());
+        kvminit();          // turn on paging
+        trapinithart();     // install kernel trap vector
+        plicinithart();     // ask PLIC for device interrupts
     }
+
+    scheduler();
 }
