afaaf73dcd4371382dfbc5c2180a7cf720038237
Yangxinzhe <1245314699@qq.com>
2025-04-19 19:35:16 +0800

    feat: add file operation

--- kernel/file.c
+++ kernel/file.c
@@ -1,11 +1,17 @@
+//
+// Support functions for system calls that involve file descriptors.
+//
+
 #include "types.h"
-#include "param.h"
 #include "riscv.h"
+#include "defs.h"
+#include "param.h"
 #include "fs.h"
 #include "spinlock.h"
-#include "defs.h"
+#include "sleeplock.h"
 #include "file.h"
 #include "stat.h"
+#include "proc.h"
 
 struct devsw devsw[NDEV]; 
 struct {
@@ -15,4 +21,85 @@ struct {
 
 void fileinit(void) {
     initlock(&ftable.lock, "ftable");
+}
+
+// Allocate a file structure.
+struct file* filealloc(void) {
+    struct file *f;
+
+    acquire(&ftable.lock);
+    for(f = ftable.file ; f < ftable.file + NFILE ; f++) {
+        if(f->ref == 0) {
+            f->ref = 1;
+            release(&ftable.lock);
+            return f;
+        }
+    }
+    release(&ftable.lock);
+    return 0;
+}
+
+
+// Increment ref count for file f
+struct file* filedup(struct file *f) {
+    acquire(&ftable.lock);
+    if(f->ref < 1)
+        panic("filedup");
+    f->ref++;
+    release(&ftable.lock);
+    return f;
+}
+
+// Close file f. (Decrement ref count, close when reaches 0.)
+void fileclose(struct file *f) {
+    struct file ff;
+
+    acquire(&ftable.lock);
+    if(f->ref < 1)
+        panic("fileclose");
+    if(--f->ref > 0) {
+        release(&ftable.lock);
+        return ;
+    }
+    ff = *f;
+    f->ref = 0;
+    f->type = FD_NONE;
+    release(&ftable.lock);
+
+    if(ff.type == FD_PIPE) {
+        // TODO: recover pipe
+        // pipeclose(ff.pipe, ff.writable);
+        printf("file.c pipeclose(ff.pipe, ff.writable);");
+    } else if(ff.type == FD_INODE || ff.type == FD_DEVICE) {
+        begin_op();
+        iput(ff.ip);
+        end_op();
+    }
+        
+}
+
+// Read from file f
+// addr is a user virtual address
+int fileread(struct file *f, uint64 addr, int n){
+    int r = 0;
+
+    if(f->readable == 0)
+        return -1;
+    
+    if(f->type == FD_PIPE) {
+        r = piperead(f->pipe, addr, n);
+    } else if (f->type == FD_DEVICE) {
+        if(f->major < 0 || f->major >= NDEV || !devsw[f->major].read)
+            return -1;
+        r = devsw[f->major].read(1, addr, n);
+    } else if (f->type == FD_INODE) {
+        ilock(f->ip);
+        if((r = readi(f->ip, 1, addr, f->off, n)) > 0)
+            f->off += r;
+        iunlock(f->ip);
+    } else {
+        panic("fileread");
+    }
+
+    return r;
 }
