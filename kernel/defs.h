1ed518f8aad499c9df0ea338189d4a53a9f93cba
Yangxinzhe <1245314699@qq.com>
2025-04-21 10:26:25 +0800

    feat: pipe

--- kernel/defs.h
+++ kernel/defs.h
@@ -1,6 +1,7 @@
 struct buf;
 struct spinlock;
 struct context;
+struct pipe;
 struct proc;
 struct inode;
 struct stat;
@@ -83,6 +84,12 @@ void            initlog(int, struct superblock*);
 void            log_write(struct buf*);
 void            begin_op(void);
 void            end_op(void);
+
+// pipe.c
+int             pipealloc(struct file**, struct file**);
+void            pipeclose(struct pipe*, int);
+int             piperead(struct pipe*, uint64, int);
+int             pipewrite(struct pipe*, uint64, int);
 
 // printf.c
 void            printf(char*, ...);
