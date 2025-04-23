de0ab53a4a389b9042cff3163e6834f08175676e
Yangxinzhe <1245314699@qq.com>
2025-04-23 17:20:04 +0800

    fix: dev interrupt initalize

--- kernel/trap.c
+++ kernel/trap.c
@@ -134,7 +134,7 @@ void kerneltrap() {
     if(intr_get() != 0)
         panic("kerneltrap: interrupts enabled");
 
-    if((which_dev == devintr()) == 0){
+    if((which_dev = devintr()) == 0){
         printf("scause %p\n", scause);
         printf("sepc = %p stval = %p\n", r_sepc(), r_stval());
         panic("kerneltrap");
