1f59add4bf22392fdff09d5b8b8a997e0cf0f41c
Yangxinzhe <1245314699@qq.com>
2025-04-23 14:55:56 +0800

    feat: console read

--- kernel/console.c
+++ kernel/console.c
@@ -134,14 +134,44 @@ void consoleintr(int c) {
     acquire(&cons.lock);
 
     switch (c) {
-    case C('P'):
+    case C('P'): // print process list
         procdump();
         break;
+    case C('U'): // kill line.
+        while(cons.e != cons.w && cons.buf[(cons.e-1) % INPUT_BUF] != '\n') {
+          cons.e--;
+          consputc(BACKSPACE);
+        }
+        break;
+    case C('H'): // Backspace
+        if(cons.e != cons.w) {
+          cons.e--;
+          consputc(BACKSPACE);
+        }
+        break;
     default:
+        if(c != 0 && cons.e - cons.r < INPUT_BUF) {
+          c = (c == '\r') ? '\n' : c;
+
+          // echo back to user.
+          consputc(c);
+
+          // store for consumption by consoleread().
+          cons.buf[cons.e++ % INPUT_BUF] = c;
+
+          if(c == '\n' || c == C('D') || cons.e == cons.r + INPUT_BUF) {
+            // wake up consoleread() if a whole line (or end-of-file)
+            // has arrived
+            cons.w = cons.e;
+            wakeup(&cons.r);
+          }
+        }
         break;
     }
     release(&cons.lock);
 }
+
+
 void consoleinit(void){
     initlock(&cons.lock, "cons");
 
