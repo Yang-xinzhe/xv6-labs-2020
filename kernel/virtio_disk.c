79f669f17366342c326189e05f174b25247a04dc
Yangxinzhe <1245314699@qq.com>
2025-04-24 10:46:23 +0800

    fix: initializing bugs fix

--- kernel/virtio_disk.c
+++ kernel/virtio_disk.c
@@ -18,46 +18,48 @@
 #include "virtio.h"
 
 // the address of virtio mmio register r.
-#define R(r)    ((volatile uint32 *)(VIRTIO0 + (r)))
+#define R(r) ((volatile uint32 *)(VIRTIO0 + (r)))
 
 static struct disk {
-    // memory for virtio description &c for queue 0.
-    // this is a global instead of allocated because it must
-    // be multiple contiguous pages,  which kalloc() 
-    // doesn't support, and page aligned.
-    char pages[2 * PGSIZE];
-    struct VRingDesc *desc;
-    uint16 *avail;
-    struct UsedArea *used;
+ // memory for virtio descriptors &c for queue 0.
+ // this is a global instead of allocated because it must
+ // be multiple contiguous pages, which kalloc()
+ // doesn't support, and page aligned.
+  char pages[2*PGSIZE];
+  struct VRingDesc *desc;
+  uint16 *avail;
+  struct UsedArea *used;
 
-    // our own book-keeping.
-    char free[NUM]; // is a descriptor free?
-    uint16 used_idx; // we've looked this far in used[2..NUM].
+  // our own book-keeping.
+  char free[NUM];  // is a descriptor free?
+  uint16 used_idx; // we've looked this far in used[2..NUM].
 
-    // track info about in-fight operations,
-    // for use when completion interrupts arrives.
-    // indexed by first descriptor index of chain.
-    struct {
-        struct buf *b;
-        char status;
-    } info[NUM];
+  // track info about in-flight operations,
+  // for use when completion interrupt arrives.
+  // indexed by first descriptor index of chain.
+  struct {
+    struct buf *b;
+    char status;
+  } info[NUM];
+  
+  struct spinlock vdisk_lock;
+  
+} __attribute__ ((aligned (PGSIZE))) disk;
 
-    struct spinlock vdisk_lock;
-} __attribute__((aligned (PGSIZE))) disk;
-
-void virtio_disk_init(void) {
+void
+virtio_disk_init(void)
+{
   uint32 status = 0;
 
   initlock(&disk.vdisk_lock, "virtio_disk");
 
-  if (*R(VIRTIO_MMIO_MAGIC_VALUE) != 0x74726976 ||
-      *R(VIRTIO_MMIO_VERSION) != 1 ||
-      *R(VIRTIO_MMIO_DEVICE_ID) != 2 ||
-      *R(VIRTIO_MMIO_VENDOR_ID) != 0x554d4551)
-  {
+  if(*R(VIRTIO_MMIO_MAGIC_VALUE) != 0x74726976 ||
+     *R(VIRTIO_MMIO_VERSION) != 1 ||
+     *R(VIRTIO_MMIO_DEVICE_ID) != 2 ||
+     *R(VIRTIO_MMIO_VENDOR_ID) != 0x554d4551){
     panic("could not find virtio disk");
   }
-
+  
   status |= VIRTIO_CONFIG_S_ACKNOWLEDGE;
   *R(VIRTIO_MMIO_STATUS) = status;
 
@@ -88,9 +90,9 @@ void virtio_disk_init(void) {
   // initialize queue 0.
   *R(VIRTIO_MMIO_QUEUE_SEL) = 0;
   uint32 max = *R(VIRTIO_MMIO_QUEUE_NUM_MAX);
-  if (max == 0)
+  if(max == 0)
     panic("virtio disk has no queue 0");
-  if (max < NUM)
+  if(max < NUM)
     panic("virtio disk max queue too short");
   *R(VIRTIO_MMIO_QUEUE_NUM) = NUM;
   memset(disk.pages, 0, sizeof(disk.pages));
@@ -100,63 +102,72 @@ void virtio_disk_init(void) {
   // avail = pages + 0x40 -- 2 * uint16, then num * uint16
   // used = pages + 4096 -- 2 * uint16, then num * vRingUsedElem
 
-  disk.desc = (struct VRingDesc *)disk.pages;
-  disk.avail = (uint16 *)(((char *)disk.desc) + NUM * sizeof(struct VRingDesc));
-  disk.used = (struct UsedArea *)(disk.pages + PGSIZE);
+  disk.desc = (struct VRingDesc *) disk.pages;
+  disk.avail = (uint16*)(((char*)disk.desc) + NUM*sizeof(struct VRingDesc));
+  disk.used = (struct UsedArea *) (disk.pages + PGSIZE);
 
-  for (int i = 0; i < NUM; i++)
+  for(int i = 0; i < NUM; i++)
     disk.free[i] = 1;
 
   // plic.c and trap.c arrange for interrupts from VIRTIO0_IRQ.
 }
 
-// find a free descriptor， mark it non-free, return its index
-static int alloc_desc() {
-    for(int i = 0 ; i < NUM ; i++) {
-        if(disk.free[i]) {
-            disk.free[i] = 0;
-            return i;
-        }
+// find a free descriptor, mark it non-free, return its index.
+static int
+alloc_desc()
+{
+  for(int i = 0; i < NUM; i++){
+    if(disk.free[i]){
+      disk.free[i] = 0;
+      return i;
     }
-    return -1;
+  }
+  return -1;
 }
 
-// mark a descriptor as free
-static void free_desc(int i) {
-    if(i >= NUM)
-        panic("virtio_disk_intr 1");
-    if(disk.free[i])
-        panic("virtio_disk_intr 2");
-    disk.desc[i].addr = 0;
-    disk.free[i] = 1;
-    wakeup(&disk.free[0]);
+// mark a descriptor as free.
+static void
+free_desc(int i)
+{
+  if(i >= NUM)
+    panic("virtio_disk_intr 1");
+  if(disk.free[i])
+    panic("virtio_disk_intr 2");
+  disk.desc[i].addr = 0;
+  disk.free[i] = 1;
+  wakeup(&disk.free[0]);
 }
 
 // free a chain of descriptors.
-static void free_chain(int i) {
-    while(1) {
-        free_desc(i);
-        if(disk.desc[i].flags & VRING_DESC_F_NEXT)
-            i = disk.desc[i].next;
-        else
-            break;
-    }
+static void
+free_chain(int i)
+{
+  while(1){
+    free_desc(i);
+    if(disk.desc[i].flags & VRING_DESC_F_NEXT)
+      i = disk.desc[i].next;
+    else
+      break;
+  }
 }
 
-static int alloc3_desc(int *idx){
-    for(int i = 0 ; i < 3 ; i++) {
-        idx[i] = alloc_desc();
-        if(idx[i] < 0) {
-            for(int j = 0 ; j < i ; j++) {
-                free_desc(idx[i]);
-            }
-            return -1;
-        }
+static int
+alloc3_desc(int *idx)
+{
+  for(int i = 0; i < 3; i++){
+    idx[i] = alloc_desc();
+    if(idx[i] < 0){
+      for(int j = 0; j < i; j++)
+        free_desc(idx[j]);
+      return -1;
     }
-    return 0;
+  }
+  return 0;
 }
 
-void virtio_disk_rw(struct buf *b, int write) {
+void
+virtio_disk_rw(struct buf *b, int write)
+{
   uint64 sector = b->blockno * (BSIZE / 512);
 
   acquire(&disk.vdisk_lock);
@@ -237,7 +248,8 @@ void virtio_disk_rw(struct buf *b, int write) {
   release(&disk.vdisk_lock);
 }
 
-void virtio_disk_intr()
+void
+virtio_disk_intr()
 {
   acquire(&disk.vdisk_lock);
 
